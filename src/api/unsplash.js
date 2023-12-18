8602c992908ab1638ca94e58008ebfadaa5c5d4f
Uyaam Maalik <81246509+uyaammaalik@users.noreply.github.com>
2023-12-18 15:07:28 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -9,10 +9,3 @@ export default axios.create({
     Authorization: `Client-ID ${apiKey}`,
   },
 });
-
-// export default axios.create({
-//   baseURL: "https://api.unsplash.com",
-//   headers: {
-//     Authorization: "Client-ID i7r0YIxDSg4La753FQFzfbhgCj0hMgGvo9H78den9MY",
-//   },
-// });
